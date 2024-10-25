200e9fa8a3ace2f7dc2a5ba23294e9447cde958e
meldy04 <120117788+meldy04@users.noreply.github.com>
2024-10-25 12:43:30 -0400

    Fixed Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -12,10 +12,11 @@ public class Main {
      */
     public static void main(String[] args) {
         final AppBuilder appBuilder = new AppBuilder();
-        final JFrame application = appBuilder.addLogoutUseCase()
+        final JFrame application = appBuilder
                                             .addLoginView()
                                             .addSignupView()
                                             .addLoggedInView()
+                                            .addLogoutUseCase()
                                             .addSignupUseCase()
                                             .addLoginUseCase()
                                             .addChangePasswordUseCase()
