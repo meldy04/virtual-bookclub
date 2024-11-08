33d3ec7cb51b5e6fe856e1cbb637305eb3abaa2c
vythili6 <yo.vythilingum@mail.utoronto.ca>
2024-11-07 20:54:36 -0500

    Created the Book and Review entities

---
+++ src/main/java/entity/Review.java
@@ -0,0 +1,64 @@
+package entity;
+
+
+/**
+ * A representation of the reviews.
+ */
+
+public class Review implements Reviews {
+
+    private User user;
+    private Book book;
+    private String text;
+    private double rating;
+
+    public Review(User user, Book book, String text, double rating) {
+        this.user = user;
+        this.book = book;
+        this.text = text;
+        this.rating = rating;
+    }
+
+    @Override
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Book getBook() {
+        return book;
+    }
+
+    @Override
+    public double getRating() {
+        return rating;
+    }
+
+    @Override
+    public void editRating(double ratings) {
+        this.rating = ratings;
+    }
+
+    @Override
+    public void editText(String texts) {
+        this.text = texts;
+    }
+
+    public String toString() {
+        final String p1 = "User" + ':' + ' ';
+        final String p2 = "Book" + ':' + ' ';
+        final String p3 = "Rating" + ':' + ' ';
+        final String p4 = "Review" + ':' + ' ';
+
+        return p1 + user.getName() + '\n' + p2 + book.getName() + '\n' + p3 + rating + '\n'
+                + p4 + '\n' + text;
+    }
+
+
+
+}
