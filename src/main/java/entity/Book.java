74f41a9a8bf8178606958d1357a3767cf61ddfe7
vythili6 <yo.vythilingum@mail.utoronto.ca>
2024-11-07 20:54:36 -0500

    Created the Book and Review entities

---
+++ src/main/java/entity/Book.java
@@ -0,0 +1,63 @@
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private String genre;
+    private double rating;
+    private List<Review> reviews;
+
+    public Book(String title, String author, String genre, double rating) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.rating = rating;
+        this.reviews = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void addReview(Review review) {
+        this.reviews.add(review);
+    }
+
+    private void setTitle(String newTitle) {
+        this.title = newTitle;
+    }
+
+    private void setAuthor(String newAuthor) {
+        this.author = newAuthor;
+    }
+
+    private void setGenre(String newGenre) {
+        this.genre = newGenre;
+    }
+
+    private void setRating(double newRating) {
+        this.rating = newRating;
+    }
+
+
+}
