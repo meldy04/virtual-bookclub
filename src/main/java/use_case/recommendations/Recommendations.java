6d22e1052ad6011a7d63c54e4845dd7bef6ca36f
meldy04 <120117788+meldy04@users.noreply.github.com>
2024-11-12 13:18:34 -0500

    Implemented Recommendations with API

--- src/main/java/use_case/recommendations/Recommendations.java
+++ src/main/java/use_case/recommendations/Recommendations.java
@@ -1,7 +1,14 @@
 package use_case.recommendations;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import entity.Book;
 import entity.User;
@@ -11,6 +18,9 @@ import entity.User;
  */
 
 public class Recommendations {
+
+    private static final int SUCCESS_CODE = 200;
+
     /**
      * RecommendedBooks function.
      * @param user User
@@ -21,11 +31,12 @@ public class Recommendations {
 
         for (Book book : user.getReadBooks()) {
             final String genre = book.getGenre();
-            final List<Book> genreRecommendations = fetchBooksFromApi(genre);
+            final List<Book> genreRecommendations = fetchBooksByGenre(genre);
 
             for (Book recommendedBook : genreRecommendations) {
                 if (!user.getReadBooks().contains(recommendedBook)
-                        && !user.getBooksToRead().contains(recommendedBook)) {
+                        && !user.getBooksToRead().contains(recommendedBook)
+                        && !recommended.contains(recommendedBook)) {
                     recommended.add(recommendedBook);
                 }
             }
@@ -33,9 +44,43 @@ public class Recommendations {
         user.getRecommendedBooks().addAll(recommended);
         return recommended;
     }
-    // TODO: Implement API integration
 
-    private List<Book> fetchBooksFromApi(String genre) {
-        return new ArrayList<>();
+    private List<Book> fetchBooksByGenre(String genre) {
+        final List<Book> books = new ArrayList<>();
+
+        try {
+            final String urlString = "https://openlibrary.org/subjects/" + genre.toLowerCase() + ".json?limit=5";
+            final URL url = new URL(urlString);
+            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            final int responseCode = conn.getResponseCode();
+            if (responseCode == SUCCESS_CODE) {
+                final StringBuilder inline = new StringBuilder();
+                final Scanner scanner = new Scanner(url.openStream());
+
+                while (scanner.hasNext()) {
+                    inline.append(scanner.nextLine());
+                }
+                scanner.close();
+
+                final JSONObject jsonResponse = new JSONObject(inline.toString());
+                final JSONArray works = jsonResponse.getJSONArray("works");
+
+                for (int i = 0; i < works.length(); i++) {
+                    final JSONObject work = works.getJSONObject(i);
+                    final String title = work.getString("title");
+                    final String author = work.getJSONArray("authors").getJSONObject(0).getString("name");
+
+                    books.add(new Book(title, author, genre, 0));
+                }
+            }
+        }
+        catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+        return books;
     }
 }
