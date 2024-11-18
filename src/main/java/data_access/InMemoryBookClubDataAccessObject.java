842bf67fdee1d9741d8c2d6f2c2f5b76f34e5583
vishraj12 <vishaka.rajani@mail.utoronto.ca>
2024-11-18 10:31:23 -0500

    Added addClub and Club Exists methods

--- src/main/java/data_access/InMemoryBookClubDataAccessObject.java
+++ src/main/java/data_access/InMemoryBookClubDataAccessObject.java
@@ -11,7 +11,7 @@ import use_case.join_club.JoinClubDataAccessInterface;
 
 /**
  * In-memory implementation of the DAO for storing BookClub data. This implementation does
- * NOT persist data between runs of the program yes love that.
+ * NOT persist data between runs of the program yes love that huh.
  */
 public class InMemoryBookClubDataAccessObject implements JoinClubDataAccessInterface, CreateClubDataAccessInterface {
 
