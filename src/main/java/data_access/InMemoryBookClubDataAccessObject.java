b108d6d310579e9e0d367fd151f8850a88cd9c13
vishraj12 <vishaka.rajani@mail.utoronto.ca>
2024-11-18 10:05:57 -0500

    Added addClub and Club Exists methods

--- src/main/java/data_access/InMemoryBookClubDataAccessObject.java
+++ src/main/java/data_access/InMemoryBookClubDataAccessObject.java
@@ -4,38 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import entity.BookClub;
-import entity.User;
+import use_case.create_club.CreateClubDataAccessInterface;
 import use_case.join_club.JoinClubDataAccessInterface;
 
 /**
  * In-memory implementation of the DAO for storing BookClub data. This implementation does
  * NOT persist data between runs of the program.
  */
-public class InMemoryBookClubDataAccessObject implements JoinClubDataAccessInterface {
+public class InMemoryBookClubDataAccessObject implements JoinClubDataAccessInterface, CreateClubDataAccessInterface {
 
-    private final Map<String, BookClub> bookClubMap;
+    private final Map<String, List<String>> bookClubMap;
 
-    public InMemoryBookClubDataAccessObject(Map<String, BookClub>bookClubMap) {
+    public InMemoryBookClubDataAccessObject(Map<String, List<String>> bookClubMap) {
         this.bookClubMap = bookClubMap;
     }
 
     @Override
-    public void addUser(User user, String clubName) {
-        bookClubMap.get(clubName).addMember(user);
+    public void addUser(String username, String clubName) {
+        bookClubMap.get(clubName).add(username);
     }
 
     @Override
-    public boolean isMember(User user, String clubName) {
-        return bookClubMap.get(clubName).getMembers().contains(user);
+    public void addClub(String clubName) {
+        bookClubMap.put(clubName, new ArrayList<>());
     }
 
     @Override
-    public List<BookClub> getAllClubs() {
-        return new ArrayList<>(bookClubMap.values());
+    public boolean clubExists(String clubName) {
+        return bookClubMap.containsKey(clubName);
     }
 
-    public Map<String, BookClub> getBookClubMap() {
+    @Override
+    public boolean isMember(String username, String clubName) {
+        return bookClubMap.get(clubName).contains(username);
+    }
+
+    public Map<String, List<String>> getBookClubMap() {
         return bookClubMap;
     }
 }
