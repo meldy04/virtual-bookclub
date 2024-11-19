d0a60ac9f34330b50d7eb4619614f791e940bf3e
Ali Hassan Shaikh <alihassan.shaikh@mail.utoronto.ca>
2024-11-19 10:56:22 -0500

    commit 2 part for jackson translator

---
+++ src/main/java/view/Join_ClubView.java
@@ -0,0 +1,18 @@
+package view;
+import javax.swing.*;
+
+public class Join_ClubView {
+
+
+    private JPanel contentPane;
+    private JTextField bookClub;
+    private JTextField instructions
+    private JComboBox bookClublist;
+    private JButton backButton1;
+    private JButton viewMembersListButton;
+    private JButton joinClubButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
