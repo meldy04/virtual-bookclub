5e6320d7e6b7dc7badf6500b0e504be3bbfdf4a6
vishraj12 <vishaka.rajani@mail.utoronto.ca>
2024-11-30 22:25:34 -0500

    CreateClubout

---
+++ src/main/java/use_case/create_club/CreateClubOutputBoundary.java
@@ -0,0 +1,13 @@
+package use_case.create_club;
+
+import use_case.create_club.CreateClubOutputData;
+
+public interface CreateClubOutputBoundary {
+
+    void prepareSuccessView(CreateClubOutputData outputData);
+
+    /**
+     * Prepares the fail view for the Join Club Use Case.
+     */
+    void prepareFailView(String errorMessage);
+}
