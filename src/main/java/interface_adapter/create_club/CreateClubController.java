2653a782a53d7b3205713503d124618d7aa61327
vishraj12 <vishaka.rajani@mail.utoronto.ca>
2024-11-30 21:56:59 -0500

    CreateClubController

---
+++ src/main/java/interface_adapter/create_club/CreateClubController.java
@@ -0,0 +1,27 @@
+package interface_adapter.create_club;
+
+import use_case.create_club.CreateClubInputBoundary;
+import use_case.create_club.CreateClubInputData;
+
+/**
+ * Controller for the Create Club Use Case.
+ */
+public class CreateClubController {
+    private final CreateClubInputBoundary createClubUseCaseInteractor;
+
+    public CreateClubController(CreateClubInputBoundary createClubUseCaseInteractor) {
+        this.createClubUseCaseInteractor = createClubUseCaseInteractor;
+    }
+
+    /**
+     * Executes the Join Club Use Case.
+     * @param clubDescription description of the club
+     * @param clubName name of the club the user wants to join
+     * @param username the username of the person creating the bookclub.
+     */
+    public void execute(String clubName, String clubDescription, String username) {
+        final CreateClubInputData inputData = new CreateClubInputData(clubName, clubDescription, username);
+        createClubUseCaseInteractor.execute(inputData);
+
+    }
+}
