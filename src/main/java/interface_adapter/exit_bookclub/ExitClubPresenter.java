ad0117f879c39f1160c241a1cca4bed25906b2ea
Ali Hassan Shaikh <alihassan.shaikh@mail.utoronto.ca>
2024-12-02 01:14:52 -0500

    review changes for recent commit

--- src/main/java/interface_adapter/exit_bookclub/ExitClubPresenter.java
+++ src/main/java/interface_adapter/exit_bookclub/ExitClubPresenter.java
@@ -1,7 +1,6 @@
 package interface_adapter.exit_bookclub;
 
 import interface_adapter.ViewManagerModel;
-import interface_adapter.change_password.LoggedInState;
 import interface_adapter.change_password.LoggedInViewModel;
 import use_case.exit_bookclub.ExitClubOutputBoundary;
 
@@ -22,9 +21,6 @@ public class ExitClubPresenter implements ExitClubOutputBoundary {
 
     @Override
     public void prepareSuccessView() {
-        final LoggedInState loggedInState = loggedInViewModel.getState();
-        loggedInViewModel.setState(loggedInState);
-        loggedInViewModel.firePropertyChanged();
         this.viewManagerModel.setState(loggedInViewModel.getViewName());
         this.viewManagerModel.firePropertyChanged();
     }
